8a6cf28a1701fc86360d1f2520e54b955fa78ac1
bbhanotr <bbhanotr@google.com>
2024-09-09 15:31:17 +0530

    Adding ALB for SSL Secured request and created ec2 instance

--- bin/agorra_cdk.js
+++ bin/agorra_cdk.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 const cdk = require('aws-cdk-lib');
-const { AgoraStack } = require('../lib/agorra-stack');
+const { Ec2Stack } = require('../lib/ec2-stack');
+const { AcmStack } = require('../lib/acm-stack');
+const { AlbStack } = require('../lib/alb-stack');
+const { Route53Stack } = require('../lib/route53-stack');
 
-// Ensure environment variables are set
+const app = new cdk.App();
+
 const account = process.env.AWS_ACCOUNT_ID;
 const region = process.env.AWS_REGION;
 
@@ -12,11 +16,37 @@ if (!account || !region) {
   process.exit(1);
 }
 
-const app = new cdk.App();
+const env = { account, region };
 
-new AgoraStack(app, 'AgoraStack', {
-  env: {
-    account: account,
-    region: region,
-  },
+const hostedZoneName = 'agorra.biz';
+const domainNames = ['ondc-buying-staging.agorra.biz'];
+
+// Create the EC2 Stack
+const ec2Stack = new Ec2Stack(app, 'Ec2Stack', { env });
+
+// Create the ACM Stack
+const acmStack = new AcmStack(app, 'AcmStack', { 
+  env,
+  hostedZoneName,
+  domainNames,
 });
+
+// Create the ALB Stack
+const albStack = new AlbStack(app, 'AlbStack', {
+  env,
+  ec2Instance: ec2Stack.ec2Instance,
+});
+
+// Create the Route 53 Stack
+const route53Stack = new Route53Stack(app, 'Route53Stack', {
+  env,
+  hostedZoneName,
+  domainNames,
+});
+
+// Set up dependencies
+albStack.addDependency(ec2Stack);
+albStack.addDependency(acmStack);
+route53Stack.addDependency(albStack);
+
+app.synth();
